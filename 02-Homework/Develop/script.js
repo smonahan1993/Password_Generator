e8fa46926037cb75a3152b199fa5f451bd7ad412
Sean Monahan <sean.r.monahan3@gmail.com>
2021-07-14 17:59:30 -0400

    rewrote with confirm

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -10,47 +10,21 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 //password generator
 function generatePassword() {
   // asks how many characters for password
-  var charCount = prompt("Between 10 and 20, how many characters would you like your password to be?");
-  if (isNaN(charCount)){
-    alert('Please choose a number');
-    return; 
-  } else if (charCount < 10 || charCount > 20){
-    alert("Must be between 10 and 20 characters");
-    return;
-  } else { 
-    // asks if function should include lowercase letters
-  var lowerQuest = prompt("Would you like to include lowercase letters? Y or N");
-   if (lowerQuest !== "Y" || lowerQuest !== "N"){
-     alert('Answer must be Y or N');
-     return;
-   } else {
-     // asks if should include uppercase letters
-  var upperQuest = prompt("Would you like to include uppercase letters? Y or N");
-  if (upperQuest !== "Y" || upperQuest !== "N"){
-    alert('Answer must be Y or N');
-    return;
-  } else {
-    // asks if password should include numeric values
-    var numerQuest = prompt("Would you like to include numeric values? Y or N");
-  if (numerQuest !== "Y" || numerQuest !== "N"){
-    alert('Answer must be Y or N');
-    return;
-  } else {
-    // asks if password should include special characters
-    var specQuest = prompt("Would you like to include numeric values? Y or N");
-  if (specQuest !== "Y" || specQuest !== "N"){
-    alert('Answer must be Y or N');
-    return;
-  }
+  let charCount = 0;
+  do {
+   charCount = prompt("Between 8 and 128, how many characters would you like your password to be?");
+
+  } while ((charCount < 8)||(charCount > 128))
   
-}
-// array of lowercase letters
+  var lowerQuest = confirm("Would you like to include lowercase letters?");
+  var upperQuest = confirm("Would you like uppercase letters included?");
+  var numerQuest = confirm("Would you like to include numeric values?");
+  var specQuest = confirm("Would you like to include special characters?");
+
+  // array of lowercase letters
 var lowerCaseLetters =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // array of uppercase letters
 var upperCaseLetters =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"];
@@ -61,21 +35,59 @@ var specChars =["!","`","~","@","#","$","%","^","&","*","(",")","_","+","="]
 //all available characters
   
 
-var dataArray = [" "]
+var dataArray = [""]
 
-if (lowerQuest === "Y") dataArray = dataArray.concat(lowerCaseLetters);
-if (upperQuest === "Y") dataArray = dataArray.concat(upperCaseLetters);
-if (numerQuest === "Y") dataArray = dataArray.concat(numerValues);
-if (specQuest === "Y") dataArray = dataArray.concat(specChars);
-  var passwordLength= 10;
+if (lowerQuest) dataArray = dataArray.concat(lowerCaseLetters);
+if (upperQuest) dataArray = dataArray.concat(upperCaseLetters);
+if (numerQuest) dataArray = dataArray.concat(numerValues);
+if (specQuest) dataArray = dataArray.concat(specChars);
+  console.log(dataArray)
   var passwordValue= "";
-  for (var i = 0; i < charCount; i++) {
+  for (var i = 0; i <= charCount; i++) {
     var randomNumber = Math.floor(Math.random() * dataArray.length);
     var randomGen = dataArray[randomNumber];
-    password.push(String.fromCharCode(randomNumber));
+    
+    passwordValue += randomGen
   }
+  return passwordValue;
+  // if (isNaN(charCount)){
+  //   alert('Please choose a number');
+  //   return; 
+  // } else if (charCount < 10 || charCount > 20){
+  //   alert("Must be between 10 and 20 characters");
+  //   return;
+  // } else { 
+  //   // asks if function should include lowercase letters
+  // var lowerQuest = prompt("Would you like to include lowercase letters? Y or N");
+  //  if (lowerQuest !== "Y" || lowerQuest !== "N"){
+  //    alert('Answer must be Y or N');
+  //    return;
+  //  } else {
+  //    // asks if should include uppercase letters
+  // var upperQuest = prompt("Would you like to include uppercase letters? Y or N");
+  // if (upperQuest !== "Y" || upperQuest !== "N"){
+  //   alert('Answer must be Y or N');
+  //   return;
+  // } else {
+  //   // asks if password should include numeric values
+  //   var numerQuest = prompt("Would you like to include numeric values? Y or N");
+  // }
+  // if (numerQuest !== "Y" || numerQuest !== "N"){
+  //   alert('Answer must be Y or N');
+  //   return;
+  // } else {
+  //   // asks if password should include special characters
+  //   var specQuest = prompt("Would you like to include numeric values? Y or N");
+  // if (specQuest !== "Y" || specQuest !== "N"){
+  //   alert('Answer must be Y or N');
+  //   return;
+  // }
+  // } 
+}
 
-  }
 
 
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
